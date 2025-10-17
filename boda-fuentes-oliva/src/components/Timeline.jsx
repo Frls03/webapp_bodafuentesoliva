49909b3d16c88abae705eaa492fc6c0631a45350
Frls03 <lafm2002@gmail.com>
2025-10-17 17:44:38 -0600

    moviendo iconos a carpeta publica "icons"

--- boda-fuentes-oliva/src/components/Timeline.jsx
+++ boda-fuentes-oliva/src/components/Timeline.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import "../styles/Timeline.css";
 
 const events = [
-  { time: "14:00", title: "Recepción", icon: "src/assets/arco.png" },
-  { time: "14:45", title: "Ceremonia", icon: "src/assets/anillos.png" },
-  { time: "15:20", title: "Fotos con los novios", icon: "src/assets/camara.png" },
-  { time: "15:45", title: "Brindis", icon: "src/assets/brindis.png" },
-  { time: "16:00", title: "Primer Baile", icon: "src/assets/baile.png" },
-  { time: "16:30", title: "Cena", icon: "src/assets/cena.png" },
-  { time: "17:30", title: "Pastel", icon: "src/assets/pastel.png" },
-  { time: "18:00", title: "Baile", icon: "src/assets/bailando.png" },
-  { time: "20:00", title: "Fin de la noche", icon: "src/assets/atardecer.png" },
+  { time: "14:00", title: "Recepción", icon: "public/icons/arco.png" },
+  { time: "14:45", title: "Ceremonia", icon: "public/icons/anillos.png" },
+  { time: "15:20", title: "Fotos con los novios", icon: "public/icons/camara.png" },
+  { time: "15:45", title: "Brindis", icon: "public/icons/brindis.png" },
+  { time: "16:00", title: "Primer Baile", icon: "public/icons/baile.png" },
+  { time: "16:30", title: "Cena", icon: "public/icons/cena.png" },
+  { time: "17:30", title: "Pastel", icon: "public/icons/pastel.png" },
+  { time: "18:00", title: "Baile", icon: "public/icons/bailando.png" },
+  { time: "20:00", title: "Fin de la noche", icon: "public/icons/atardecer.png" },
 ].sort((a, b) => {
   const timeA = a.time.split(':').map(Number);
   const timeB = b.time.split(':').map(Number);
